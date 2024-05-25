5eb3e4234534105d26de16e1fd95fa9af48e3e6d
Юра <yusckorobogatov@yandex.ru>
2024-05-25 15:26:51 +0400

    Выполнена домашняя работа к семинару 2

--- src/sem2/Main.java
+++ src/sem2/Main.java
@@ -1,19 +1,11 @@
 package sem2;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args){
+        Pair<Integer, String> person1 = new Pair<>(1, "Иванов Иван");
+        Pair<Integer, String> person2 = new Pair<>(2, "Петров Петр");
+        Pair<Integer, String> person3 = new Pair<>(3, "Викторов Виктор");
+        System.out.println("\nСПИСОК СТУДЕНТОВ:");
+        System.out.println(person1 + "\n" + person2 + "\n" + person3);
     }
 }
---
+++ src/sem2/Pair.java
@@ -0,0 +1,31 @@
+package sem2;
+
+/*
+Реализуйте простой обобщённый класс, такой как пара или кортеж.
+ Затем создайте обобщенный метод, который работает с этим классом.
+  Например, вы можете создать метод, который принимает пару
+   и возвращает их сумму или конкатенацию.
+
+ */
+
+public class Pair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    public Pair(T1 key, T2 value) {
+        this.first = key;
+        this.second = value;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public String toString(){
+        return first + ". " + second + ".";
+    }
+}
